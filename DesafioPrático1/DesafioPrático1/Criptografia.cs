19f49ff31aa91eba2af2edd1b8bc25791e926f72
LucasGuerra2003 <101567946+LucasGuerra2003@users.noreply.github.com>
2023-11-08 15:41:33 -0300

    Add files via upload

---
+++ DesafioPrático1/DesafioPrático1/Criptografia.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Linq;
+using System.IO;
+
+namespace DesafioPrático1
+{
+    static class Criptografia
+    {
+        public static byte[] CapturaChave(string chavecapturada)
+        {
+           SHA256 sha256 = SHA256.Create();
+            return sha256.ComputeHash(Encoding.UTF8.GetBytes(chavecapturada));
+        }
+
+        public static string criptografiaAES(string senha, byte[] chavecriptografia)
+        {
+            Aes aesAlg = Aes.Create();
+
+            aesAlg.Key = chavecriptografia;
+            aesAlg.IV = new byte[16];
+
+            ICryptoTransform Criptografia = aesAlg.CreateEncryptor();
+
+            byte[] Criptografado;
+
+            MemoryStream MemoriaStream = new MemoryStream();
+            CryptoStream CriptografiaStream = new CryptoStream(MemoriaStream, Criptografia, CryptoStreamMode.Write);
+            StreamWriter SWCriptografar = new StreamWriter(CriptografiaStream);
+            
+            try
+            {
+ 
+            }
+            finally
+            {
+                if (SWCriptografar != null)
+                    SWCriptografar.Close();
+            }
+
+            Criptografado = MemoriaStream.ToArray();
+
+
+            return Convert.ToBase64String(Criptografado);
+        }
+
+        public static string criptografiaRSA(string senha, RSAParameters ParametrosRSA)
+        {
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSA.ImportParameters(ParametrosRSA);
+            byte[] data = Encoding.UTF8.GetBytes(senha);
+            byte[] encryptedData = RSA.Encrypt(data, true);
+            return Convert.ToBase64String(encryptedData);
+        }
+
+        public static string CriptografiaSHA256(string senha, string chave)
+        {
+            string chavesenha = senha + chave;
+            var sha256Hash = SHA256.Create();
+            var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(chavesenha));
+            var build = new StringBuilder();
+
+            foreach (var bts in bytes)
+            {
+                build.Append(bts.ToString("x2"));
+            }
+
+            return build.ToString();
+
+
+        }
+    }
+}
---
+++ DesafioPrático1/DesafioPrático1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DesafioPrático1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string chaveSecreta = "#modalGR#GPTW#top#maiorEmpresaTecnologia#baixadaSantista";
+            byte[] bytesChaves = Criptografia.CapturaChave(chaveSecreta);
+
+            int i = 1;
+            string senha = "admin";
+            Console.WriteLine("Chave Secreta para Criptografia: " + chaveSecreta);
+
+            Console.WriteLine("Senha para Criptografia: " + senha + "\n");
+
+
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSAParameters parametrosRSA = RSA.ExportParameters(true);
+
+            string senhaRSA = Criptografia.criptografiaRSA(senha, parametrosRSA);
+            Console.WriteLine("Criptografia usando RSA: " + senhaRSA + "\n");
+
+            string senhaAES = Criptografia.criptografiaAES(senha, bytesChaves);
+            Console.WriteLine("Criptografada usando AES: " + senhaAES + "\n");
+
+            string senhaCriptografadaSHA2563 = Criptografia.CriptografiaSHA256(senha, chaveSecreta);
+            Console.WriteLine("Criptografada usando SHA-256: " + senhaCriptografadaSHA2563 + "\n");
+
+        }
+    }
+}
